1f6da00132b0d6adeba05555f5eb853f2cfcaa6c
dell <2451513186@qq.com>
2019-11-17 18:09:55 +0800

    111111

---
+++ src/main/java/com/springboot/springbootfirst/bean/User.java
@@ -0,0 +1,50 @@
+package com.springboot.springbootfirst.bean;
+
+
+public class User {
+    private Integer id;
+    private String name;
+    private int age;
+    private String email;
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/main/java/com/springboot/springbootfirst/controller/DispatcherController.java
@@ -0,0 +1,11 @@
+package com.springboot.springbootfirst.controller;
+
+
+import com.springboot.springbootfirst.bean.User;
+
+
+public class DispatcherController {
+    public static void main(String[] args) {
+
+    }
+}
