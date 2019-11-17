0a073cb54cf20f84e192336cf05efb5fd00f4901
dell <2451513186@qq.com>
2019-11-17 18:22:38 +0800

    update user

--- src/main/java/com/springboot/springbootfirst/bean/User.java
+++ src/main/java/com/springboot/springbootfirst/bean/User.java
@@ -8,9 +8,6 @@ public class User {
     private String email;
     private String address;
 
-    public Integer getId() {
-        return id;
-    }
 
     public void setId(Integer id) {
         this.id = id;
